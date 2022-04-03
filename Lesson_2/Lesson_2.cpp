57ae0e59cb20f5ce2caf153f2bdfbe3465d6cab0
Alexandr <kajlaleksandr1986@gmail.com>
2022-04-03 08:12:19 +0700

    commit

--- Lesson_2/Lesson_2.cpp
+++ Lesson_2/Lesson_2.cpp
@@ -4,13 +4,18 @@ Lesson_2::Lesson_2(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
-    model = new QStandardItemModel(this);
-    model->appendRow(new QStandardItem(QIcon("icon/C++.png"), "C++"));
-    model->appendRow(new QStandardItem(QIcon("icon/Python.png"), "Python"));
-    model->appendRow(new QStandardItem(QIcon("icon/C.png"), "C"));
-    model->appendRow(new QStandardItem(QIcon("icon/JavaScript.png"), "JavaScript"));
-    model->appendRow(new QStandardItem(QIcon("icon/C#.png"), "C#"));
-    model->appendRow(new QStandardItem(QIcon("icon/Kotlin.png"), "Kotlin"));
-    model->appendRow(new QStandardItem(QIcon("icon/Ruby.png"), "Ruby"));
+    QStringList lst;
+    lst << "C++" << "Python" << "C" << "JavaScript" << "C#" << "Kotlin" << "Ruby";;
+
+    model = new QStandardItemModel(lst.size(), 1);
+    for (int i = 0; i < model->rowCount(); ++i)
+    {
+        QModelIndex index = model->index(i, 0);
+        model->setData(index, lst.at(i), Qt::DisplayRole);
+        model->setData(index, QIcon("icon/"+lst.at(i)+".png"), Qt::DecorationRole);
+    }
+
+    ui.listView->setViewMode(QListView::IconMode);
     ui.listView->setModel(model);
+
 }
